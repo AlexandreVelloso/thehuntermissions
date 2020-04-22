c7906c6a496887c32418be928681a0be5d8888fe
Alexandre Velloso <xande.velloso@gmail.com>
2020-04-22 16:57:56 -0300

    Add animal page

---
+++ frontend/src/pages/Animal/index.js
@@ -0,0 +1,160 @@
+import React, { useState, useEffect } from 'react';
+import { FiRefreshCcw } from 'react-icons/fi';
+import { useHistory, useParams } from 'react-router-dom';
+
+import './styles.css';
+import NavBar from '../../components/Navbar';
+import Checkbox from '../../components/Checkbox';
+
+import api from '../../services/api';
+
+export default function Animal() {
+    const token = localStorage.getItem('token');
+    const username = localStorage.getItem('username');
+    const history = useHistory();
+    const { animalId } = useParams();
+
+    const [animal, setAnimal] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        async function loadAnimal() {
+            try {
+                setLoading(true);
+                const response = await api.get(`animals/${animalId}`, {
+                    headers: {
+                        Authorization: token,
+                    }
+                });
+                setLoading(false);
+
+                setAnimal(response.data);
+            } catch (err) {
+                if (!err.response) {
+                    alert('Error when try to connect to server');
+                } else if (err.response.status === 401) {
+                    history.push('/login');
+                }
+            }
+        }
+
+        loadAnimal();
+    }, [token, history, animalId]);
+
+    async function loadAnimal() {
+        try {
+            setLoading(true);
+            const response = await api.get(`animals/${animalId}`, {
+                headers: {
+                    Authorization: token,
+                }
+            });
+            setLoading(false);
+
+            setAnimal(response.data);
+        } catch (err) {
+            if (!err.response) {
+                alert('Error when try to connect to server');
+            } else if (err.response.status === 401) {
+                history.push('/login');
+            }
+        }
+    }
+
+    async function updateObjective(objectiveId, completed) {
+        try {
+            await api.put(`objectives/${objectiveId}`, {
+                completed,
+            }, {
+                headers: {
+                    Authorization: token,
+                }
+            });
+        }
+        catch (err) {
+
+        }
+    }
+
+    async function updateMission(missionId) {
+        try {
+            await api.put(`missions/${missionId}`, {
+                completed: true,
+            }, {
+                headers: {
+                    Authorization: token,
+                }
+            });
+        }
+        catch (err) {
+
+        }
+    }
+
+    async function handleChangeObjective(objectiveId, checked) {
+        setLoading(true);
+        await updateObjective(objectiveId, checked);
+        await loadAnimal();
+        setLoading(false);
+    }
+
+    function changeObjectivesCheckbox(missionId) {
+        const animalChanged = animal.missions.map((mission) => {
+
+            if (mission.id === missionId) {
+                const objectives = mission.objectives.map((objective) => {
+                    objective.completed = true;
+                    return objective;
+                });
+
+                mission.objectives = objectives;
+            }
+
+            return mission;
+        });
+
+        setAnimal(animalChanged);
+    }
+
+    async function handleCompleteMission(missionId) {
+        setLoading(true);
+        changeObjectivesCheckbox(missionId);
+        await updateMission(missionId);
+        await loadAnimal();
+        setLoading(false);
+    }
+
+    return (
+        <div>
+            <NavBar username={username}></NavBar>
+            <div className="loader-icon">
+                <div className={loading ? 'loading' : 'not-loading'}><FiRefreshCcw size={45}></FiRefreshCcw></div>
+            </div>
+            <div className="animal-container">
+                <div className="animal-name">
+                    <h1>{animal.name}</h1>
+                </div>
+                <ul>
+                    {animal.missions && animal.missions.map(mission => (
+                        <li key={mission.id}>
+                            <div className="mission-title">
+                                <h2>{mission.name}</h2>
+                                <button onClick={() => handleCompleteMission(mission.id)} disabled={loading}>Complete mission</button>
+                            </div>
+                            <div className="mission-info">
+                                <strong>{mission.reward}$gm</strong>
+                            </div>
+                            <ul>
+                                {mission.objectives.map(objective => (
+                                    <li key={objective.id}>
+                                        <Checkbox id={objective.id} label={objective.name} checked={objective.completed} onChange={handleChangeObjective}></Checkbox>
+                                    </li>
+                                ))}
+                            </ul>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </div>
+    );
+}
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Home from './pages/Home';
+import Animal from './pages/Animal';
 
 export default function Routes({ match }) {
     return (
@@ -12,6 +13,7 @@ export default function Routes({ match }) {
                 <Route path="/" exact component={Home}></Route>
                 <Route path="/login" exact component={Login}></Route>
                 <Route path="/register" exact component={Register}></Route>
+                <Route path="/animal/:animalId" component={Animal}></Route>
             </Switch>
         </BrowserRouter>
     );
