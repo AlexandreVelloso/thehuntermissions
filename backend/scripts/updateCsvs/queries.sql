4d4afe7cc1b84863b532f32edc5d3b4fc6f820a0
Alexandre Velloso <xande.velloso@gmail.com>
2020-06-10 15:26:21 -0300

    Update comments

--- backend/scripts/updateCsvs/queries.sql
+++ backend/scripts/updateCsvs/queries.sql
@@ -1,14 +1,14 @@
 -- Users
 SELECT * FROM users;
 
--- User Weapons
+-- User Objectives
 SELECT u.email, o.name AS objective_name, completed, count(o.name) AS count_duplicated
 FROM users u
 JOIN user_objectives uo ON u.id = uo.user_id
 JOIN objectives o ON o.id = uo.objective_id
 GROUP BY email, name, completed;
 
--- User objectives
+-- User Weapons
 SELECT u.id, u.email, weapon_id, w.name AS weapon_name, have_weapon
 FROM users u
 JOIN user_weapons uw ON u.id = uw.user_id
