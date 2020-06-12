6eb74078a94f34c785fef1ccf8d2da2277e3540d
Alexandre Velloso <xande.velloso@gmail.com>
2020-06-12 10:31:13 -0300

    Update token config for dev environment

--- backend/api/utils/jwtToken.js
+++ backend/api/utils/jwtToken.js
@@ -1,5 +1,8 @@
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
-module.exports.sign = (data, options = { expiresIn: '5m' }) => jwt.sign(data, process.env.SECRET, options);
-module.exports.verify = (token) => jwt.verify(token, process.env.SECRET);
+const expiresIn = process.env.EXPIRES_IN || '365d';
+const secret = process.env.SECRET || 'secret';
+
+module.exports.sign = (data, options = { expiresIn }) => jwt.sign(data, secret, options);
+module.exports.verify = (token) => jwt.verify(token, secret);
