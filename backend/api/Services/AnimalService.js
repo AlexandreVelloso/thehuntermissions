bb15e58545de981f06c37cb7fc97dcd715bd740f
Alexandre Velloso <xande.velloso@gmail.com>
2020-06-11 22:53:15 -0300

    Optimize animals queries

--- backend/api/Services/AnimalService.js
+++ backend/api/Services/AnimalService.js
@@ -4,27 +4,65 @@ const MissionService = require('../Services/MissionService');
 
 module.exports = {
     async index(userId) {
-        const animals = await Animal.query();
-
-        for(let index = 0; index < animals.length; index += 1){
-            const animalId = animals[index].id;
-
-            animals[index].missions = await MissionService.getMissionsByAnimal(animalId, userId);
-        }
+        const animals = await Animal.query()
+            .select('animals.*')
+            .withGraphFetched('missions')
+            .modifyGraph('missions', (builder) => {
+                builder.select('missions.*')
+                    .withGraphFetched('objectives')
+                    .modifyGraph('objectives', (builder) => {
+                        builder.select('objectives.*', 'user_objectives.user_id', 'user_objectives.completed')
+                            // eslint-disable-next-line func-names
+                            .leftJoin('user_objectives', function () {
+                                this.on('objectives.id', 'user_objectives.objective_id')
+                                    .on('user_objectives.user_id', userId);
+                            })
+                            .withGraphFetched('weapons')
+                            .modifyGraph('weapons', (builder) => {
+                                builder.select('weapons.*', 'user_weapons.have_weapon')
+                                    // eslint-disable-next-line func-names
+                                    .leftJoin('user_weapons', function () {
+                                        this.on('weapons.id', 'user_weapons.weapon_id')
+                                            .on('user_weapons.user_id', userId);
+                                    });
+                            })
+                    })
+            })
 
         return animals;
     },
 
     async get(animalId, userId) {
         const animal = await Animal.query()
-            .where('id', animalId)
+            .select('animals.*')
+            .withGraphFetched('missions')
+            .modifyGraph('missions', (builder) => {
+                builder.select('missions.*')
+                    .withGraphFetched('objectives')
+                    .modifyGraph('objectives', (builder) => {
+                        builder.select('objectives.*', 'user_objectives.user_id', 'user_objectives.completed')
+                            // eslint-disable-next-line func-names
+                            .leftJoin('user_objectives', function () {
+                                this.on('objectives.id', 'user_objectives.objective_id')
+                                    .on('user_objectives.user_id', userId);
+                            })
+                            .withGraphFetched('weapons')
+                            .modifyGraph('weapons', (builder) => {
+                                builder.select('weapons.*', 'user_weapons.have_weapon')
+                                    // eslint-disable-next-line func-names
+                                    .leftJoin('user_weapons', function () {
+                                        this.on('weapons.id', 'user_weapons.weapon_id')
+                                            .on('user_weapons.user_id', userId);
+                                    });
+                            })
+                    })
+            })
+            .where('animals.id', animalId)
             .first();
 
         if (!animal) {
             throw new EntityNotFoundException('Animal not found');
         }
-
-        animal.missions = await MissionService.getMissionsByAnimal(animal.id, userId);
 
         return animal;
     },
