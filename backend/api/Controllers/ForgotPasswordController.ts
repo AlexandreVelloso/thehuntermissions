713c3cfd490195a8d505b79923226aa07d34e08e
Alexandre Velloso <xande.velloso@gmail.com>
2020-06-19 10:14:26 -0300

    Refactor ForgorPassword, add repository

--- backend/api/Controllers/ForgotPasswordController.ts
+++ backend/api/Controllers/ForgotPasswordController.ts
@@ -4,11 +4,19 @@ import ForgotPasswordService from '../Services/ForgotPasswordService';
 import ErrorHandlerMiddleware from '../Middleware/ErrorHandlerMiddleware';
 
 class ForgotPasswordController {
-    static async sendEmail(req: Request, res: Response) {
+
+    private forgorPasswordService: ForgotPasswordService;
+
+    public constructor(forgorPasswordService: ForgotPasswordService) {
+        this.forgorPasswordService = forgorPasswordService;
+    }
+
+    async sendEmail(req: Request, res: Response) {
         const { email = '' } = req.body;
 
         try {
-            await ForgotPasswordService.sendEmail(email);
+            await this.forgorPasswordService
+                .sendEmail(email);
             return res.status(200).end();
         } catch (err) {
             return ErrorHandlerMiddleware.handle(err, req, res);
--- backend/api/Routes/ForgotPasswordRoutes.ts
+++ backend/api/Routes/ForgotPasswordRoutes.ts
@@ -1,9 +1,17 @@
 import { Router } from 'express';
 
 import ForgotPasswordController from '../Controllers/ForgotPasswordController';
+import ForgotPasswordServiceImpl from '../Services/ForgotPasswordServiceImpl';
+import UserRepositoryImpl from '../Repositories/UserRepositoryImpl';
 
 const router = Router();
 
-router.post('/forgotPassword', ForgotPasswordController.sendEmail);
+const forgotPasswordController = new ForgotPasswordController(
+    new ForgotPasswordServiceImpl(
+        new UserRepositoryImpl()
+    )
+);
+
+router.post('/forgotPassword', (req, res) => forgotPasswordController.sendEmail(req, res));
 
 export default router;
