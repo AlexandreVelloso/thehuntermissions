cb1c042c296b58e41dc1c48843287d6745ea18d2
Alexandre Velloso <xande.velloso@gmail.com>
2020-06-18 21:28:27 -0300

    Refactor objective routes

--- backend/api/Routes/ObjectiveRoutes.ts
+++ backend/api/Routes/ObjectiveRoutes.ts
@@ -4,8 +4,10 @@ import ObjectiveController from '../Controllers/ObjectiveController';
 
 const router = Router();
 
-router.get('/objectives', ObjectiveController.index);
-router.get('/objectives/:id', ObjectiveController.get);
-router.put('/objectives/:id', ObjectiveController.update);
+const objectiveController = new ObjectiveController();
+
+router.get('/objectives', (req, res) => objectiveController.index(req, res));
+router.get('/objectives/:id', (req, res) => objectiveController.get(req, res));
+router.put('/objectives/:id', (req, res) => objectiveController.update(req, res));
 
 export default router;
