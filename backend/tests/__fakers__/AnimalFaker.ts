2a6aa944f4dde86c5d913a484941a1f985ad5969
Alexandre Velloso <xande.velloso@gmail.com>
2020-06-29 17:03:40 -0300

    Update Ids in Faker functions

--- backend/tests/__fakers__/AnimalFaker.ts
+++ backend/tests/__fakers__/AnimalFaker.ts
@@ -2,9 +2,10 @@ import faker from 'faker';
 
 import AnimalModel from '../../database/models/AnimalModel';
 
-export async function generateAnimal() {
+export async function generateAnimal(animalId?: number) {
     return await AnimalModel.query()
         .insert({
+            id: animalId,
             name: faker.name.firstName(),
         });
 }
--- backend/tests/__fakers__/WeaponFaker.ts
+++ backend/tests/__fakers__/WeaponFaker.ts
@@ -5,7 +5,7 @@ import WeaponModel from '../../database/models/WeaponModel';
 export async function generateWeapon(weaponId?: number) {
     return await WeaponModel.query()
         .insert({
-            id: weaponId ? weaponId : faker.random.number(100),
+            id: weaponId,
             name: faker.name.firstName(),
             price: faker.random.number(1000),
         });
