54a738d64f6417a37cfa27dbf9dee1da55eb8523
Alexandre Velloso <xande.velloso@gmail.com>
2020-06-29 16:47:32 -0300

    Update objective e2e tests

--- backend/tests/__fakers__/ObjectiveFaker.ts
+++ backend/tests/__fakers__/ObjectiveFaker.ts
@@ -2,11 +2,12 @@ import faker from 'faker';
 
 import ObjectiveModel from '../../database/models/ObjectiveModel';
 
-export async function generateObjective(missionId?: number) {
+export async function generateObjective(objectiveId?: number, missionId?: number) {
     const missionIdDb = missionId ? missionId : faker.random.number(100);
 
     return await ObjectiveModel.query()
         .insert({
+            id: objectiveId,
             name: faker.name.firstName(),
             mission_id: missionIdDb,
             created_at: '',
