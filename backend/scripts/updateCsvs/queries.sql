c668291dbd3bf7c15b3bbad8f331ea211b30b66d
Alexandre Velloso <xande.velloso@gmail.com>
2020-07-01 22:28:51 -0300

    Add SQL to retrieve User Equipaments

--- backend/scripts/updateCsvs/queries.sql
+++ backend/scripts/updateCsvs/queries.sql
@@ -13,3 +13,9 @@ SELECT u.id, u.email, weapon_id, w.name AS weapon_name, have_weapon
 FROM users u
 JOIN user_weapons uw ON u.id = uw.user_id
 JOIN weapons w ON w.id = uw.weapon_id;
+
+-- User Equipaments
+SELECT u.id, u.email, equipament_id e.name AS equipament_name, have_equipament
+from users u
+JOIN user_equipaments ue ON u.id = ue.user_id
+JOIN equipaments e ON e.id = ue.equipament_id
